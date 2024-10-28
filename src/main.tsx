5fdee6bb3f72da9190c5ad7f52a727bab445fe9c
Rober Guerrero <tj.rober.guerrero.m@upds.net.bo>
2024-10-28 18:40:32 -0400

    fix project images

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { App } from './App'
 import './index.css'
 import '@fontsource-variable/onest';
-import 'flowbite';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
